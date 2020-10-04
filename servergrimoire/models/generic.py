8e539fc3026d927552cabcaee04ebbeac293526f
Fundor333 <fundor333@fundor333.com>
2020-10-04 21:16:40 +0200

    Adding some base module

--- servergrimoire/models/generic.py
+++ servergrimoire/models/generic.py
@@ -1,7 +1,6 @@
 from colorfield.fields import ColorField
 from django.db import models
 
-# Create your models here.
 
 class Label(models.Model):
     name = models.CharField(max_length=300)
@@ -11,9 +10,29 @@ class Label(models.Model):
     def __str__(self):
         return self.name
 
+
 class IpModelMixin:
-    internal_ip= models.GenericIPAddressField(null=True, blank=True)
-    external_ip= models.GenericIPAddressField(null=True, blank=True)
+    internal_ip = models.GenericIPAddressField(null=True, blank=True)
+    external_ip = models.GenericIPAddressField(null=True, blank=True)
+
 
 class LabelMixin:
     labels = models.ManyToManyField("Label")
+
+
+class Domain(LabelMixin, models.Model):
+    domain_name = models.GenericIPAddressField(null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True)
+    ssl_valid = models.BooleanField(default=False)
+    ssl_end_date = models.DateField(null=True,blank=True)
+    domain_end_date  = models.DateField(null=True,blank=True)
+
+    def __str__(self):
+        return f"{self.domain_name} {self.ip}"
+
+
+class Server(LabelMixin, IpModelMixin, models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"Server {self.name}, local Ip {self.internal_ip}"
