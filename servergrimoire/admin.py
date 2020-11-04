43bdc1f9b9a09fa256e5b770a40065601fe542d6
Fundor333 <fundor333@fundor333.com>
2020-11-04 15:16:54 +0100

    Adding code to the admin

--- servergrimoire/admin.py
+++ servergrimoire/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
-# Register your models here.
+from servergrimoire.models import Server, GitLabInstallation
+from servergrimoire.task import (
+    task_dns_check,
+    task_ssl_check,
+    Domain,
+    task_gitlab,
+)
+
+
+def task_dns_check_admin(modeladmin, request, queryset):
+    for e in queryset:
+        task_dns_check(e)
+
+
+def task_ssl_check_admin(modeladmin, request, queryset):
+    for e in queryset:
+        task_ssl_check(e)
+
+
+def task_gitlab_admin(modeladmin, request, queryset):
+    for e in queryset:
+        task_gitlab(e)
+
+
+task_dns_check_admin.short_description = "Run DNS check"
+task_ssl_check_admin.short_description = "Run SSL check"
+task_gitlab_admin.short_description = "Run Gitlab check"
+
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ["domain_name", "ip"]
+    search_fields = ["domain_name", "ip"]
+    ordering = ["domain_name"]
+    actions = [task_dns_check_admin, task_ssl_check_admin]
+
+
+class GitLabInstallationAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+    list_display = ["domain_name", "ip"]
+    search_fields = ["domain_name", "ip"]
+
+
+class ServerAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+    list_display = ["base_url", "version", "revision", "token"]
+    search_fields = ["base_url", "version", "revision", "token"]
+    actions = [task_gitlab_admin]
+
+
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(Server, ServerAdmin)
+admin.site.register(GitLabInstallation, GitLabInstallationAdmin)
